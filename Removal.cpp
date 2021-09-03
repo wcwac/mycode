329326575ded56afea054a9f16aa248cd293ad2b
wcwac <16619878+wcwac@users.noreply.github.com>
2021-09-03 18:42:20 +0800

    upload code

---
+++ Removal.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+int a[N],last[N];
+int f[N][11];
+int main()
+{
+    cin.tie(0)->sync_with_stdio(false);
+    int n, m, k;
+    cin >> n >> m >> k;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        
+    }
+    return 0;
+}
