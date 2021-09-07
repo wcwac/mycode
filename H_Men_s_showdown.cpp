7f3ab1dd3c6f852edba6e7da19a4180a9daf3f70
wcwac <16619878+wcwac@users.noreply.github.com>
2021-09-07 19:43:30 +0800

    upload code

---
+++ H_Men_s_showdown.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int _[100010];
+
+int dfs(int x)
+{
+    if (x == 1)
+        return 0;
+    if (_[x] != -1)
+        return _[x];
+    if (x =)
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+    }
+
+    return 0;
+}
