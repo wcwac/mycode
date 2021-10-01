6271cbf0ee8447e0af80595a371fc6bfa8711c50
wcwac <16619878+wcwac@users.noreply.github.com>
2021-10-01 19:16:39 +0800

    upload code

---
+++ F_Zebraness.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 110;
+ll f[N][N][2];
+char a[N][N];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i] + 1;
+    }
+    for (int i=1;i<=n;++i){
+        for (int j=1;j<=n;++j){
+            if (a[i][j]=='?'){
+                
+            }
+        }
+    }
+
+
+    return 0;
+}
