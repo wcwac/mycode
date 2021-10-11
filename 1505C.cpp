0f2a05e82d33ce7c12444eaaf5afea986171fce7
wcwac <16619878+wcwac@users.noreply.github.com>
2021-10-11 18:36:30 +0800

    upload code

---
+++ 1505C.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 2; i < s.length(); ++i)
+    {
+        if (s[i] - 'A' != (s[i - 1] - 'A' + s[i - 2] + 'A') % 26)
+        {
+            return puts("NO"), 0;
+        }
+    }
+    puts("YES");
+}
