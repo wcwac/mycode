6054007ed652ce787bd156ec79ae137c2b36e4ec
wcwac <16619878+wcwac@users.noreply.github.com>
2021-11-11 22:04:15 +0800

    upload code

---
+++ H_SBC_s_Hangar.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 60;
+long long a[N];
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+    cin >> l >> r;
+    return 0;
+}
