c210f08136eb39b4745d1287eb94710cbdfd3c23
wcwac <16619878+wcwac@users.noreply.github.com>
2022-02-15 19:38:13 +0800

    upload code

---
+++ Descent_of_Dragons.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+using namespace __gnu_cxx;
+using namespace std;
+
+map<int, rope<int>> v;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i)
+    {
+        v[0].push_back(i);
+    }
+    while (m--){
+        
+    }
+
+    return 0;
+}
